9e4e0a90a73c3d0e56c5f0c3522187f715df9ce6
henriquenm <marculo.henrique@gmail.com>
2016-02-02 15:28:26 -0200

    workaround case options hash is empty

--- lib/acts_as_tenant/model_extensions.rb
+++ lib/acts_as_tenant/model_extensions.rb
@@ -80,8 +80,13 @@ module ActsAsTenant
 
         reflect_on_all_associations(:belongs_to).each do |a|
           unless a == reflect_on_association(tenant) || polymorphic_foreign_keys.include?(a.foreign_key)
-            binding.pry
-            association_class =  a.options[:class_name].nil? ? a.name.to_s.classify.constantize : a.options[:class_name].constantize
+
+            association_class =  if a.options[:class_name].nil? 
+              a.name.to_s.classify.constantize rescue false || "Spree::#{a.name.to_s.classify}".constantize
+            else
+              a.options[:class_name].constantize
+            end
+
             validates_each a.foreign_key.to_sym do |record, attr, value|
               primary_key = if association_class.respond_to?(:primary_key)
                               association_class.primary_key
